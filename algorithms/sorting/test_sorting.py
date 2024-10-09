01e44dafd8c113554fc7cdd0100b5bd1ae9a906b
Florian <florian@flos-pc.fritz.box>
2024-10-09 18:22:18 +0200

    Add first code and pre-commit file

---
+++ algorithms/sorting/test_sorting.py
@@ -0,0 +1,7 @@
+# algorithms/sorting/test_sorting.py
+from bubble_sort import bubble_sort
+
+def test_bubble_sort():
+    assert bubble_sort([3, 2, 1]) == [1, 2, 3]
+    assert bubble_sort([]) == []
+    assert bubble_sort([1]) == [1]
